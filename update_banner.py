c0b1c0d262daed4ba3ba210ed4064bdd38b423f8
Syed <syed@apache.org>
2021-03-19 10:43:35 -0400

    Add markdown and severity to messages

--- update_banner.py
+++ update_banner.py
@@ -8,9 +8,11 @@ password_decoded = unquote(os.environ.get('DB_PASSWORD'))
 conn = pymysql.connect(host=os.environ.get('DB_HOST'),
             user=os.environ.get('DB_USER'),
             password=password_decoded,
-            db=os.environ.get('DB_NAME'))
+            db=os.environ.get('DB_NAME'),
+            cursorclass=pymysql.cursors.DictCursor)
 
 MESSAGE = os.environ.get('MESSAGE')
+SEVERITY = os.environ.get('SEVERITY', 'info')
 
 
 
@@ -19,7 +21,11 @@ def update_banner():
         with conn.cursor() as cur:
             cur.execute('DELETE FROM messages WHERE 1=1')
             if MESSAGE:
-                cur.execute('INSERT INTO messages (content) VALUES (%s)', (MESSAGE))
+                cur.execute("SELECT id from mediatype WHERE name='text/markdown'")
+                result = cur.fetchone()
+                media_type_id = int(result['id'])
+
+                cur.execute('INSERT INTO messages (content, media_type_id, severity) VALUES (%s, %d, %s)', (MESSAGE, media_type_id, SEVERITY))
 
         conn.commit()
     except Exception as e:
